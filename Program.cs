40604e8e861d7384e3afa6ff0f80bb50c861380a
Dipak Bari <dipakbari4@gmail.com>
2020-07-19 23:13:05 +0530

    Basic variable assignment and printing to the console

--- Program.cs
+++ Program.cs
@@ -6,7 +6,20 @@ namespace Variables
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int firstVariable = 27;
+            int secondVariable = 45;
+            float bodyTemp = 98.6f;
+            double weight = 53.356d;
+            string name = "Dipak";
+            char input = 'y';
+            bool isHoliday = true;
+
+            Console.WriteLine("Hello " + name + "! your age is " + firstVariable + 
+                ", you're expecting the score of " + secondVariable + 
+                ", seems your body temparature is " + bodyTemp + " but your weight " + weight + 
+                " your input for holiday is " + isHoliday + " which is equal to " + input);
+
+            Console.ReadKey();
         }
     }
 }
